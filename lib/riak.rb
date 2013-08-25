c3cb90b7d6c35e56ea9139edfcbc55dbd804a4df
Randy Secrist <randy.secrist@gmail.com>
2013-08-25 10:57:45 -0600

    fill out some impl

--- lib/riak.rb
+++ lib/riak.rb
@@ -1,2 +1,52 @@
+module RiakAPI
+  class Bucket
+  end
+
+  class Key
+  end
+
+  class Value
+  end
+
+  module Rest
+    # HTTP Header Model
+    class Header
+    end
+
+    class Response
+      # status, key, value
+    end
+
+    def get(bucket, key)
+    end
+    def put(bucket, key, value, headers = nil)
+      # consult strategy
+      # put to riak
+      # return response
+    end
+    def delete(bucket, key)
+    end
+    def post(bucket, key, value, headers = nil)
+    end
+  end
+end
+
 class Riak
+  include RiakAPI::Rest
+  def initialize(config)
+    @config = config
+  end
+end
+
+# Abstract Class for Execution
+class ExecutionStrategy
+end
+
+# Serial Strategy for API Invocation
+class Serial < ExecutionStrategy
+end
+
+# Concurrent Strategy for API Invocation
+# (Basic | Simple | Alternate | TBD)
+class Concurrent < ExecutionStrategy
 end
